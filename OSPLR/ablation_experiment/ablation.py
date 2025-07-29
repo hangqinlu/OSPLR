1df2e19cc6587746b7f8e120a7d4c13f9615b28a
hangqinlu <hangqinlu2@gmail.com>
2025-07-29 10:05:20 +0800

    Update ablation.py

--- OSPLR/ablation_experiment/ablation.py
+++ OSPLR/ablation_experiment/ablation.py
@@ -7,8 +7,8 @@ from typing import List
 from Ablation.DS_API import call_openai
 from Ablation.ablation_prompt import build_prompt
 def main():
-    SRC_TXT = r"C:\Users\卢航青\PycharmProjects\pythonProject11\SPLR实验数据\15%扰动未处理句子.txt"  # 输入的纯文本文件，每行一个待处理句子
-    OUTPUT_JSON = r"C:\Users\卢航青\PycharmProjects\pythonProject11\知识库书籍\模式1\实验2——10/15.jsonl"  # 最终将所有行的提取结果写入此 JSON 文件
+    SRC_TXT = r"data\group1.txt"  
+    OUTPUT_JSON = "Experimental Results1\ablation_pred/ablation_group1.json"  
     os.makedirs(Path(OUTPUT_JSON).parent, exist_ok=True)
     all_results: List[List[dict]] = []
     with open(SRC_TXT, "r", encoding="utf-8") as f:
